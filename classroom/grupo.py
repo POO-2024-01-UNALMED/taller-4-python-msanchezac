e1417a130acf81db80d0bd74c7ccdec3aad94e4e
msanchezac <msanchezac@unal.edu.co>
2024-03-03 17:21:39 -0500

    Intento 2

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -14,12 +14,12 @@ class Grupo:
 
     def agregarAlumno(self, alumno, lista=None):
         if lista is None:
-            lista=[]
+            lista=self.listadoAlumnos
         lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+        self.listadoAlumnos = lista
 
     def __str__(self):
-         return f"Grado {self.grado}"
+         return f"Grupo de estudiantes: {self._grupo}"
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
